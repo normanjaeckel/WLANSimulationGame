d5ffdc0e166da8efba497c3df592a4de5160d6c2
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-09-11 14:30:23 +0200

    Add card titles

--- wlan_simulation_game/card/admin.py
+++ wlan_simulation_game/card/admin.py
@@ -6,4 +6,8 @@ from django.contrib import admin
 from .models import Card
 
 
-admin.site.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'target', 'value', 'used')
+
+
+admin.site.register(Card, CardAdmin)
