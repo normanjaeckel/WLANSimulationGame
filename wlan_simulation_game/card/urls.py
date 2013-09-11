9819f8962678fd79483d334f83362a0970d4cd18
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-09-11 18:36:23 +0200

    Enhance card app. Fix some small things.

--- wlan_simulation_game/card/urls.py
+++ wlan_simulation_game/card/urls.py
@@ -3,25 +3,19 @@
 
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.views.generic import ListView
 
-from .models import Card
-from .views import CardDetailView, CardPlayView
+from .views import CardListView, CardDetailView, CardPlayView
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', login_required(ListView.as_view(model=Card)), name='card_list'),
+    url(r'^$', login_required(CardListView.as_view()), name='card_list'),
     url(r'^(?P<pk>\d+)/$', login_required(CardDetailView.as_view()), name='card_detail'),
     url(r'^(?P<pk>\d+)/play/$', login_required(CardPlayView.as_view()), name='card_play'),
     url(r'^print/list/$',
-        login_required(ListView.as_view(
-            model=Card,
-            template_name='card/card_list_print_list.html')),
+        login_required(CardListView.as_view(template_name='card/card_list_print_list.html')),
         name='card_list_print_list'),
     url(r'^print/all/$',
-        user_passes_test(lambda user: user.is_staff)(ListView.as_view(
-            model=Card,
+        user_passes_test(lambda user: user.is_staff)(CardListView.as_view(
             template_name='card/card_list_print_all.html')),
-        name='card_list_print_all'),
-)
+        name='card_list_print_all'))
--- wlan_simulation_game/card/views.py
+++ wlan_simulation_game/card/views.py
@@ -6,11 +6,24 @@ from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
-from django.views.generic import DetailView, RedirectView
+from django.views.generic import ListView, DetailView, RedirectView
 
 from wlan_simulation_game.exceptions import WLANSimulationGameError
 
 from .models import Card
+
+
+class CardListView(ListView):
+    """
+    View to see all cards.
+    """
+    model = Card
+
+    def get_queryset(self, *args, **kwargs):
+        """
+        Sort cards by owner and target but then shuffle them.
+        """
+        return super(CardListView, self).get_queryset(*args, **kwargs).order_by('owner', 'target', '?')
 
 
 class CardDetailView(DetailView):
@@ -25,11 +38,10 @@ class CardDetailView(DetailView):
         Method to check that only staff or owners can see their cards.
         """
         dispatch = super(CardDetailView, self).dispatch(request, *args, **kwargs)
-        if request.user.is_staff or request.user.player == self.object.owner:
-            return dispatch
-        else:
+        if not request.user.is_staff and not request.user.player == self.object.owner:
             messages.error(request, _('You are not owner of this card, so you are not allowed to see it.'))
             raise PermissionDenied
+        return dispatch
 
 
 class CardPlayView(RedirectView):
@@ -55,5 +67,5 @@ class CardPlayView(RedirectView):
         except WLANSimulationGameError, error_message:
             messages.error(self.request, error_message)
         else:
-            messages.success(self.request, _('Card %(name)s was successfully played.') % {'name': card.name})
+            messages.success(self.request, _('Card "%(name)s" was successfully played.') % {'name': card.name})
         return reverse('card_list')
