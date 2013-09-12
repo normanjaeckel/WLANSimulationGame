d5e588eb89a9e2ca3ecf07bc90a29b5d9cbbf861
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-09-12 23:40:15 +0200

    Several changes. Thanks to Stefan and Anja for testing.

--- wlan_simulation_game/player/admin.py
+++ wlan_simulation_game/player/admin.py
@@ -8,7 +8,7 @@ from .models import Player
 
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('character', 'score')
+    list_display = ('character', 'score', 'playable_cards')
 
 
 admin.site.register(Player, PlayerAdmin)
--- wlan_simulation_game/templatetags/templatetags/wlan_simulation_game_extras.py
+++ wlan_simulation_game/templatetags/templatetags/wlan_simulation_game_extras.py
@@ -4,6 +4,8 @@
 from django import template
 from django.core.urlresolvers import reverse
 
+from constance import config
+
 
 register = template.Library()
 
@@ -17,6 +19,15 @@ def url_href_class_current(context, url_name):
     url = reverse(url_name)
     html_tag_attributes = 'href="%s"' % url
     if (context['request'].path.startswith(url) and
-        (context['request'].path == '/' or not url == '/')):
+            (context['request'].path == '/' or not url == '/')):
         html_tag_attributes += ' class="current"'
     return html_tag_attributes
+
+
+@register.filter
+def rest_interceptions(count):
+    """
+    Returns the differenc between the maximum number of interceptions the the
+    given value.
+    """
+    return config.number_of_interceptions - count
