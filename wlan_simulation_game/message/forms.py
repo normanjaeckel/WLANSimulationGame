33bbc703d535d2e03adf9f3cf4b162845ccfb4bc
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-09-16 01:18:20 +0200

    Add time limit check and check for the same recipient.

--- wlan_simulation_game/message/forms.py
+++ wlan_simulation_game/message/forms.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datetime
+
 from django import forms
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import ugettext as _, ugettext_lazy
+from django.utils.timezone import localtime, now
 
 from wlan_simulation_game.player.models import Player
 
@@ -26,11 +29,27 @@ class MessageCreateForm(forms.ModelForm):
         """
         Manipulate recipient field to exclude the request user player. The
         request object is excluded from the kwargs for it was hacked in in
-        the view.
+        the view. It is attached to the form for later use.
         """
+        self.request = request
         return_value = super(MessageCreateForm, self).__init__(*args, **kwargs)
-        self.fields['recipient'].queryset = Player.objects.exclude(pk=request.user.player.pk)
+        self.fields['recipient'].queryset = Player.objects.exclude(pk=self.request.user.player.pk)
         return return_value
+
+    def clean(self, *args, **kwargs):
+        """
+        Checks the time limit for messages to the same recipient.
+        """
+        cleaned_data = super(MessageCreateForm, self).clean(*args, **kwargs)
+        minutes_to_wait = 3
+        time_limit = localtime(now()) - datetime.timedelta(minutes=minutes_to_wait)
+        if Message.objects.filter(
+                sender=self.request.user.player,
+                recipient=cleaned_data['recipient'],
+                sending_time__gt=time_limit.time()).exists():
+            raise forms.ValidationError(
+                _('You have to wait %d minutes before you can send the next message to the same recipient.') % minutes_to_wait)
+        return cleaned_data
 
 
 class MessageCreateFormStaff(forms.ModelForm):
@@ -58,6 +77,15 @@ class MessageCreateFormStaff(forms.ModelForm):
         in the view.
         """
         return super(MessageCreateFormStaff, self).__init__(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        """
+        Checks that the sender is not the recipient.
+        """
+        cleaned_data = super(MessageCreateFormStaff, self).clean(*args, **kwargs)
+        if cleaned_data['sender'] and cleaned_data['recipient'] == cleaned_data['sender']:
+            raise forms.ValidationError(_('You can not send a message from a player to himself.'))
+        return cleaned_data
 
 
 class InterceptionWizardFormOne(forms.Form):
