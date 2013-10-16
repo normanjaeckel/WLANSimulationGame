3584da932da33646259f9d34a2d85e74b6b9904f
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-10-16 16:52:12 +0200

    Insert view to print the introduction.

--- wlan_simulation_game/urls.py
+++ wlan_simulation_game/urls.py
@@ -3,6 +3,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import user_passes_test
 from django.views.generic import TemplateView
 
 
@@ -12,6 +13,10 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^print/$',
+        user_passes_test(lambda user: user.is_staff)(TemplateView.as_view(
+            template_name='home_print.html')),
+        name='home_print'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^player/', include('wlan_simulation_game.player.urls')),
