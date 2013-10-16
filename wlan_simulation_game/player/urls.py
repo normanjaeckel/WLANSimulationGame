60653f4793ccc21472e873d433b41a6a02a8b804
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2013-10-16 16:34:50 +0200

    Update Django version. Insert auto reload on message to be printed view. Add player list for print.

--- wlan_simulation_game/player/urls.py
+++ wlan_simulation_game/player/urls.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.generic import ListView, DetailView
 
 from .models import Player
@@ -12,4 +12,8 @@ urlpatterns = patterns(
     '',
     url(r'^$', login_required(ListView.as_view(model=Player)), name='player_list'),
     url(r'^(?P<pk>\d+)/$', login_required(DetailView.as_view(model=Player)), name='player_detail'),
-)
+    url(r'^print/$',
+        user_passes_test(lambda user: user.is_staff)(ListView.as_view(
+            model=Player,
+            template_name='player/player_list_print.html')),
+        name='player_list_print'))
