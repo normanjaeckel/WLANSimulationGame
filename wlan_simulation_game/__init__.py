5da639d1cc05b017ccb3d775542d0a80ce34cc4b
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2014-07-03 14:31:15 +0200

    Release 1.0.1

--- wlan_simulation_game/__init__.py
+++ wlan_simulation_game/__init__.py
@@ -4,6 +4,6 @@
 WLAN Simulation Game
 """
 
-__version__ = '1.0.1-dev'
+__version__ = '1.0.1'
 __author__ = 'Norman Jäckel <wlansimulationgame@normanjaeckel.de>'
-__date__ = 'unreleased'
+__date__ = '2014-07-03'
