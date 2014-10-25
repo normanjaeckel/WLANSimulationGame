1fcba4b8889ffd9d8090aec544fab15313c6753e
Norman Jäckel <wlansimulationgame@normanjaeckel.de>
2014-10-25 21:12:53 +0200

    Release 2.0.0

--- wlan_simulation_game/__init__.py
+++ wlan_simulation_game/__init__.py
@@ -5,7 +5,7 @@ A system for WLAN supported simulation games. It is optimized for 15 to 25
 players in five groups. See README.rst for more information.
 """
 
-__version__ = '2.0.0-dev'
+__version__ = '2.0.0'
 __author__ = 'Norman Jäckel <wlansimulationgame@normanjaeckel.de>'
-__date__ = 'unknown'
+__date__ = '2014-10-25'
 __credits__ = 'Philipp Augustin, Oskar Hahn, Friedrich Porsch'
